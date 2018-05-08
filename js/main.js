ffcb114c6d70a895c8f060e81636d11f99801789
XiaohuiSu <564970381@qq.com>
2018-05-08 14:41:51 +0800

    22

--- js/main.js
+++ js/main.js
@@ -6,17 +6,29 @@ $(document).ready(function() {
 });
 
 $(window).load(function() {
-	$.ajax({
-				type:"post",
-				url:"http://172.17.164.36:8080/blog/View.do",
-				data: {
-					page: 1
-				},
-				dataType: "json",
-				success: function(msg) {
-					alert(msg[0].id);
+	var t=new XMLHttpRequest,
+			 	obj;
+			t.onreadystatechange=function() {
+				if(t.readyState==4&&t.status==200)
+				{
+					obj=JSON.parse(this.responseText);
+					alert(obj[0].id);
 				}
-			});
+			}
+			t.open("POST","http://172.17.164.36:8080/blog/View.do",true);
+			t.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+			t.send("page=1");
+// 	$.ajax({
+// 				type:"post",
+// 				url:"http://172.17.164.36:8080/blog/View.do",
+// 				data: {
+// 					page: 1
+// 				},
+// 				dataType: "json",
+// 				success: function(msg) {
+// 					alert(msg[0].id);
+// 				}
+// 			});
 	
 });
 
