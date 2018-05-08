6eedd3a7a7ab9414930aeb84fe1d3da8d02f9ef7
XiaohuiSu <564970381@qq.com>
2018-05-08 20:00:45 +0800

    w

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ document.querySelector(".ye input").onchange = function() {
    					
    				}
    			}
-   			t.open("POST","blog/View.do",true);
+   			t.open("POST","http://172.17.164.36:8080/blog/View.do",true);
    			t.setRequestHeader("Content-type","application/x-www-form-urlencoded");
    			t.send("page=1");
    }
@@ -85,8 +85,9 @@ $("#myInput").bind('input propertychange',function() {
 		document.querySelector("#myFa").removeAttribute("disabled");
 	}
 });
-
+alert(document.querySelector(".he2 span").value);
 document.querySelector("#myFa").onclick = function() {
+			var c = document.querySelector("#myInput");
 	   		var t=new XMLHttpRequest,
    				obj;
    			t.onreadystatechange=function() {
@@ -94,28 +95,13 @@ document.querySelector("#myFa").onclick = function() {
    				{
    					obj=JSON.parse(this.responseText);
    					
-   					var name = document.querySelectorAll(".name");
-   					var time = document.querySelectorAll(".time");
-   					var content = document.querySelectorAll(".remen-inner p");
-   					var zhuan = document.querySelectorAll(".zhuan");
-   					var ping = document.querySelectorAll(".ping");
-   					var dian = document.querySelectorAll(".dian");
    					
-   					for(var i=0; i<10; ++i) {
-   						name[i].innerHTML = obj[i].nickName;
-   						time[i].innerHTML = obj[i].createTime;
-   						content[i].innerHTML = obj[i].content;
-   						zhuan[i].innerHTML = obj[i].repostCount;
-   						ping[i].innerHTML = obj[i].commentCount;
-   						dian[i].innerHTML = obj[i].upvoteCount;
-   						
-   					}
-   					
+   					alert("BIngo");
    				}
    			}
-   			t.open("POST","http://172.17.164.36:8080/blog/View.do",true);
+   			t.open("POST","http://172.17.164.36:8080/blog/Pub.do",true);
    			t.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-   			t.send("page="+c.value);
+   			t.send("content="+c.value+"name="+document.querySelector(".he2 span").value);
 	
 };
 
