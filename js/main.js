843019b97b29a3bc0e72ef3d8580cdd358b9e907
XiaohuiSu <564970381@qq.com>
2018-05-08 17:20:15 +0800

    0

--- js/main.js
+++ js/main.js
@@ -5,24 +5,40 @@ $(document).ready(function() {
 	});
 });
 
-// $("#myFa").click(function() {
-// 
-// });
-window.onload = function() {
-		var t=new XMLHttpRequest,
-				obj;
-			t.onreadystatechange=function() {
-				if(t.readyState==4&&t.status==200)
-				{
-					obj=JSON.parse(this.responseText);
-					alert(obj[0].id);
-				}
-			}
-			t.open("POST","http://172.17.164.36:8080/blog/View.do",true);
-			t.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-			t.send("page=1");
-}
 
+   window.onload = function() {
+   		var t=new XMLHttpRequest,
+   				obj;
+   			t.onreadystatechange=function() {
+   				if(t.readyState==4&&t.status==200)
+   				{
+   					obj=JSON.parse(this.responseText);
+   					
+   					var name = document.querySelectorAll(".name");
+   					var time = document.querySelectorAll(".time");
+   					var content = document.querySelectorAll(".remen-inner p");
+   					var zhuan = document.querySelectorAll(".zhuan");
+   					var ping = document.querySelectorAll(".ping");
+   					var dian = document.querySelectorAll(".dian");
+   					
+   					for(var i=0; i<10; ++i) {
+   						name[i].innerHTML = obj[i].nickName;
+   						time[i].innerHTML = obj[i].createTime;
+   						content[i].innerHTML = obj[i].content;
+   						zhuan[i].innerHTML = obj[i].repostCount;
+   						ping[i].innerHTML = obj[i].commentCount;
+   						dian[i].innerHTML = obj[i].upvoteCount;
+   						
+   					}
+   					
+   					alert(obj[0].id);
+   				}
+   			}
+   			t.open("POST","http://172.17.164.36:8080/blog/View.do",true);
+   			t.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+   			t.send("page=1");
+   }
+// 
 
 $("#myInput").bind('input propertychange',function() {
 	var text = $("#myInput").val();
