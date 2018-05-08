7954bd5b0d44404961ae6535b23669b12decc7ae
XiaohuiSu <564970381@qq.com>
2018-05-08 23:05:35 +0800

    v

--- js/main.js
+++ js/main.js
@@ -230,3 +230,36 @@ document.querySelector(".he2 a").onclick = function() {
 	
 };
 
+document.querySelector(".name").onclick = function() {
+			Zhong.status = 0;
+		var t=new XMLHttpRequest,
+   				obj;
+   			t.onreadystatechange=function() {
+   				if(t.readyState==4&&t.status==200)
+   				{
+   					obj=JSON.parse(this.responseText);
+   					
+   					var name = document.querySelectorAll(".name");
+   					var time = document.querySelectorAll(".time");
+   					var content = document.querySelectorAll(".remen-inner p");
+   					var zhuan = document.querySelectorAll(".zhuan");
+   					var ping = document.querySelectorAll(".ping");
+   					var dian = document.querySelectorAll(".dian");
+   					
+   					for(var i=0; i<10; ++i) {
+   						name[i].innerHTML = obj[i].nickName;
+   						time[i].innerHTML = obj[i].createTime;
+   						content[i].innerHTML = obj[i].content;
+   						zhuan[i].innerHTML = obj[i].repostCount;
+   						ping[i].innerHTML = obj[i].commentCount;
+   						dian[i].innerHTML = obj[i].upvoteCount;
+   						
+   					}
+   					
+   				}
+   			}
+   			t.open("POST","http://172.17.164.36:8080/blog/View.do",true);
+   			t.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+   			t.send("page=1&ishot=false&"+"name="+document.querySelector(".name").value);
+};
+
