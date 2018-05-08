0024a81e9c7dddfe3ebb9a961cf2e0220d990449
XiaohuiSu <564970381@qq.com>
2018-05-08 14:49:01 +0800

    4

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 	});
 });
 
-$(window).load(function() {
+$("#myFa").click(function() {
 	var t=new XMLHttpRequest,
-			 	obj;
+				obj;
 			t.onreadystatechange=function() {
 				if(t.readyState==4&&t.status==200)
 				{
@@ -18,6 +18,21 @@ $(window).load(function() {
 			t.open("POST","http://172.17.164.36:8080/blog/View.do",true);
 			t.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 			t.send("page=Bingo");
+});
+
+// $(window).load(function() {
+// 	var t=new XMLHttpRequest,
+// 			 	obj;
+// 			t.onreadystatechange=function() {
+// 				if(t.readyState==4&&t.status==200)
+// 				{
+// 					obj=JSON.parse(this.responseText);
+// 					alert(obj);
+// 				}
+// 			}
+// 			t.open("POST","http://172.17.164.36:8080/blog/View.do",true);
+// 			t.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+// 			t.send("page=Bingo");
 // 	$.ajax({
 // 				type:"post",
 // 				url:"http://172.17.164.36:8080/blog/View.do",
@@ -30,7 +45,7 @@ $(window).load(function() {
 // 				}
 // 			});
 	
-});
+// });
 
 $("#myInput").bind('input propertychange',function() {
 	var text = $("#myInput").val();
